3e7b0f65bc7a9d0b4f0a2b9eb118daabb9ba647b
Celia Rodriguez <celiro02@gmail.com>
2022-10-02 23:03:35 +0200

    fonts

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/App.scss";
+import "./fonts/Roboto/roboto-regular.ttf";
 import App from "./components/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
